9d375b05e98eaab4816a058fed1be5ba7bc08107
zmckevitt <zack.mckevitt@gmail.com>
2023-03-05 17:13:50 -0700

    small change

--- hashtable.h
+++ hashtable.h
@@ -1,8 +1,20 @@
+/*
+    Hash table implementation
+    Zack McKevitt
+*/
+
+// TODO
+// need hash table with better typing, perhaps implicit?
+// For now, just doing string prototype
+// Unions to emulate polymorphism?
+// https://stackoverflow.com/questions/10718150/c-unions-and-polymorphism
+
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #include "linkedlist.h"
 
@@ -15,8 +27,31 @@ struct ht ht_create(size_t size) {
     struct ht new_ht;
     new_ht.size = size;
     new_ht.table = malloc(size * sizeof(struct sll));
+
+    for(size_t i=0; i<size; ++i) {
+        struct sll sll_ht = sll_create();
+        new_ht.table[i] = sll_ht;
+    }
+
     return new_ht;
 };
+
+/*
+    HASH FUNCTIONS
+*/
+
+/*
+    djb2 hash function:
+    http://www.cse.yorku.ca/~oz/hash.html
+*/
+uint32_t djb2_hash(const char* str) {
+    uint32_t hash = 5381;
+    int c;
+    
+    while(c = *str++)
+        hash = ((hash << 5) + hash) + c;
+    return hash;
+}
 
 int ht_destroy(struct ht* hashtable) {
     int status = 0;
